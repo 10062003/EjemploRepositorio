b9f1d5fa5211581da61f7a8d2d0e7119b76ce548
Hogar <Hogar@Hogar-PC>
2021-04-26 07:48:43 -0500

    He colocado hola personas en el pograma

--- src/ejemplo/repositorio/EjemploRepositorio.java
+++ src/ejemplo/repositorio/EjemploRepositorio.java
@@ -9,6 +9,7 @@ public class EjemploRepositorio {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Personas");
     }
     
 }
